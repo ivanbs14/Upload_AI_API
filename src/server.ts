5e06df0096c0fbc8094bd5871d8bb639df45c8ae
Ivan Barbosa <ivanbarbosag@gmail.com>
2023-09-13 16:00:46 -0300

    Config conexion Prisma and rota getAllPrompts

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import { fastify } from "fastify";
+import { getAllPromptsRoute } from "./routes/getAllPrompts";
 
 const app = fastify()
 
-app.get('/', () => {
-   return 'Hello'
-})
+app.register(getAllPromptsRoute);
 
 app.listen({
    port: 3333,
